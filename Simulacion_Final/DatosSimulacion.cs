14d3b11b2f25d163ddcebd4514c9c83c1fb2df79
CesarLepeITT <l22212360@tijuana.tecnm.mx>
2025-12-03 18:37:52 -0800

    Implementado clase contenedor DatosSimulacion

--- Simulacion_Final/DatosSimulacion.cs
+++ Simulacion_Final/DatosSimulacion.cs
@@ -8,5 +8,14 @@ namespace Simulacion_Final
 {
     internal class DatosSimulacion
     {
+        // Parámetros de entrada
+        public int SemillaX0 { get; set; }
+        public int MultiplicadorA { get; set; }
+        public int ModuloM { get; set; }
+        public int ConstanteAditivaC { get; set; }
+
+        // Resultado (La lista de números generados)
+        public List<double> ListaNumeros { get; set; } = new List<double>();
     }
 }
+
