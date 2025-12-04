deba17633cdeae6ed3a7810c190ce492a9ccb87b
CesarLepeITT <l22212360@tijuana.tecnm.mx>
2025-12-03 18:37:08 -0800

    Implementado GeneradorNumerosPseudoaleatorios

--- Simulacion_Final/GeneradorNumerosPseudoaleatorios.cs
+++ Simulacion_Final/GeneradorNumerosPseudoaleatorios.cs
@@ -10,8 +10,28 @@ namespace Simulacion_Final
     {
         // Se encarga de generar los números pseudoaleatorios según los parámetros dados y escribirlos en un archivo csv ademas de dar la capacidad de
         // leer números pseudoaleatorios desde un archivo json
-               
 
+        public DatosSimulacion GenerarNumeros(int xo, int a, int c, int m, int cantidad)
+        {
+            DatosSimulacion resultado = new DatosSimulacion();
+
+            // Guardamos los parámetros en el modelo
+            resultado.SemillaX0 = xo;
+            resultado.MultiplicadorA = a;
+            resultado.ModuloM = m;
+            resultado.ConstanteAditivaC = c;
+
+            // Congruencial Mixto
+            double actual = xo;
+            for (int i = 0; i < cantidad; i++) 
+            {
+                actual = (a * actual + c) % m;
+                double ri = actual / m; 
+                resultado.ListaNumeros.Add(ri); 
+            }
+
+            return resultado; 
+        }
 
     }
 }
